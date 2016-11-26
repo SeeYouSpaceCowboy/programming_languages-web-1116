fa37787cc5061124bf9ead2a72e092a0d8412f61
Mohammed Chisti <93mchistI@gmail.com>
2016-11-25 20:42:47 -0500

    Completed

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,61 @@
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+
+  languages.each{|key, value|
+    value.each{|key, value|
+      new_hash[key] = value
+    }
+  }
+
+  languages.each{|key, value|
+    if key == :oo
+      new_hash.each{|key, value|
+        if languages[:oo].has_key?(key)
+          new_hash[key][:style] = [:oo]
+        end
+      }
+
+    elsif key == :functional
+      new_hash.each{|key, value|
+        if languages[:functional].has_key?(key) && !new_hash[key].has_key?(:style)
+          new_hash[key][:style] = [:functional]
+        elsif languages[:functional].has_key?(key)
+          new_hash[key][:style].push(:functional)
+        end
+      }
+    end
+  }
+
+  new_hash
 end
+
+# {
+#   :ruby => {
+#     :type => "interpreted",
+#     :style => [:oo]
+#   },
+#   :javascript => {
+#     :type => "interpreted",
+#     :style => [:oo, :functional]
+#   },
+#   :python => {
+#     :type => "interpreted",
+#     :style => [:oo]
+#   },
+#   :java => {
+#     :type => "compiled",
+#     :style => [:oo]
+#   },
+#   :clojure => {
+#     :type => "compiled",
+#     :style => [:functional]
+#   },
+#   :erlang => {
+#     :type => "compiled",
+#     :style => [:functional]
+#   },
+#   :scala => {
+#     :type => "compiled",
+#     :style => [:functional]
+#   }
+# }
